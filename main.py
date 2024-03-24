0b99a58d4a213a7b74f7cd44a2ff280bd1826252
4117 Burov Nikita Michailovich <nik_buro_0@mail.ru>
2024-03-25 01:38:33 +0300

    small demonstration graph was added

--- main.py
+++ main.py
@@ -5,13 +5,39 @@ import time
 import matplotlib.pyplot as plt
 
 
-print("Hello World")
+# size = 10
+# my_matrix = [0] * size
+# for i in range(size):
+#     my_matrix[i] = [0] * size
+#
+# my_edges = []
+# my_weights = []
+# for i in range(size):
+#     for j in range(i, size):
+#         my_matrix[i][j] = my_matrix[j][i] = random.randint(0, 3)
+#         if my_matrix[i][j] != 0:
+#             my_edges.append((i, j))
+#             my_weights.append(my_matrix[i][j])
 
-size = 10
-arr = []
-for i in range(size):
-    arr.append(random.randint(0, 10))
-print(arr)
+my_edges = []
+my_weights = []
+my_graph = [[0, 3, 6, 1, 0, 0],
+            [3, 0, 0, 0, 8, 5],
+            [6, 0, 0, 4, 4, 0],
+            [1, 0, 4, 0, 12, 0],
+            [0, 8, 4, 12, 0, 3],
+            [0, 5, 0, 0, 3, 0]]
+for i in range(len(my_graph)):
+    for j in range(0, i):
+        if my_graph[i][j] != 0:
+            my_edges.append((i, j))
+            my_weights.append(my_graph[i][j])
+start_node = 0
+end_node = 4
+length, path = dijkstra_algorithm(my_graph, start_node, end_node)
+
+print(f'Длина пути от {start_node} до {end_node} равна {length}\n путь выглядит так: {path}')
+draw_graph(len(my_graph), my_edges, my_weights, "kk")
 
 
 size_arr = []
@@ -51,8 +77,8 @@ for M in range(5, 25, 2):
         #         print(Matrix[i])
         #     draw_graph(M**2, edges, weights, layout="grid")
 
-        start_node = 0
-        end_node = 11
+        start_node = random.randint(0, M)
+        end_node = random.randint(0, M)
         start_time = time.time()
         length, path = dijkstra_algorithm(Matrix, start_node, end_node)
         end_time = time.time()
